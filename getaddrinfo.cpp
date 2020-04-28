c6265cf367ecc139bdb1b0bfb4db9faa87da47e7
SpiritCroc <dev@spiritcroc.de>
2020-04-28 16:43:02 +0300

    Toggle-able adblock hosts file [1/3]
    Change-Id: I0f5ab2ec4a6f1855abd65f371a8960087db49994

--- getaddrinfo.cpp
+++ getaddrinfo.cpp
@@ -65,6 +65,8 @@
 #include "resolv_cache.h"
 #include "resolv_private.h"
 
+#include "gethostsfile.h"
+
 #define ANY 0
 
 using android::net::Experiments;
@@ -1530,7 +1532,7 @@ static int dns_getaddrinfo(const char* name, const addrinfo* pai,
 
 static void _sethtent(FILE** hostf) {
     if (!*hostf)
-        *hostf = fopen(_PATH_HOSTS, "re");
+        *hostf = fopen(gethostsfile(), "re");
     else
         rewind(*hostf);
 }
@@ -1553,7 +1555,7 @@ static struct addrinfo* _gethtent(FILE** hostf, const char* name, const struct a
     assert(name != NULL);
     assert(pai != NULL);
 
-    if (!*hostf && !(*hostf = fopen(_PATH_HOSTS, "re"))) return (NULL);
+    if (!*hostf && !(*hostf = fopen(gethostsfile(), "re"))) return (NULL);
 again:
     if (!(p = fgets(hostbuf, sizeof hostbuf, *hostf))) return (NULL);
     if (*p == '#') goto again;
